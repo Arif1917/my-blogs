55a44ddf44fdc4bb19b91ffe3f4b6d4498ec11b4
Arif1917 <mdarifhosen1917@gmail.com>
2025-04-20 11:03:06 +0600

    for card added in the ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,32 @@
 
+import { Suspense } from 'react'
 import './App.css'
+import Blog from './components/Blog/Blog'
+import Blogs from './components/Blogs/Blogs'
+import Navbar from './components/Navbar/Navbar'
 
 function App() {
 
 
   return (
     <>
-      
-      <h1>Vite + React</h1>
+    <div>
+    <Navbar></Navbar>
+    
+
+    <div className="main-container flex text-center mt-10">
+      {/* left container */}
+      <div className="left-container w-[70%]">
+
+       <Blogs></Blogs>
+       
+      </div>
+      <div className='right-container w-[30%] '>
+           <h1 className='font-semibold text-3xl'>Reading Time:</h1>
+           <h1 className='font-semibold text-3xl'>Bookmark count: 0</h1>
+      </div>
+    </div>
+    </div>
      
     </>
   )
---
+++ src/components/Blogs/Blogs.jsx
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from 'react';
+import Blog from '../Blog/Blog';
+
+const Blogs = () => {
+ 
+    const [blogs, setBlogs] =useState([])
+
+    useEffect( () =>{
+        fetch("blogs.json")
+        .then(res => res.json())
+        .then( data => setBlogs(data))
+    },[])
+
+// console.log(blogs)
+    return (
+        <div>
+            <h1 className='font-semibold text-3xl'>Total Blogs: {blogs.length}</h1>
+            <div className='grid grid-cols-2'>
+                {
+                    blogs.map(blog => <Blog key={blog.id} blog={blog}></Blog>)
+                }
+            </div>
+        </div>
+        
+    );
+};
+
+export default Blogs;
